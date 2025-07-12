bc6f5099eceb9a755a69b6f0f73d02ef8d1f74e1
Tonpcg <welintonpc7@gmail.com>
2025-07-12 17:52:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ const hqs = [
     pdf: "https://archive.org/download/Superman_1/Superman_1.pdf"
   },
   {
-    nome: "Batman (Detective Comics #27)",
+    nome: "Detective Comics #27 (Batman)",
     imagem: "https://upload.wikimedia.org/wikipedia/en/0/0d/Detective_Comics_27.jpg",
-    pdf: "https://archive.org/download/DetectiveComics27/DetectiveComics27.pdf"
+    pdf: "https://archive.org/download/Detective_Comics_27/Detective_Comics_27.pdf"
   },
   {
     nome: "Wonder Woman #1",
     imagem: "https://upload.wikimedia.org/wikipedia/en/f/f8/Wonder_Woman_1.jpg",
-    pdf: "https://archive.org/download/WonderWoman1/WonderWoman1.pdf"
+    pdf: "https://archive.org/download/Wonder_Woman_1/Wonder_Woman_1.pdf"
   },
   {
     nome: "The Spirit #1",
@@ -25,9 +25,29 @@ const hqs = [
     pdf: "https://archive.org/download/TheSpirit1/TheSpirit1.pdf"
   },
   {
-    nome: "Captain Marvel #1",
+    nome: "Captain Marvel (Whiz Comics #2)",
     imagem: "https://upload.wikimedia.org/wikipedia/en/d/d7/Captain_Marvel_Whiz_Comics_No_1.jpg",
-    pdf: "https://archive.org/download/WhizComics_v1n02/WhizComics_v1n02.pdf"
+    pdf: "https://archive.org/download/WhizComicsNo.2/WhizComicsNo.2.pdf"
+  },
+  {
+    nome: "Flash Comics #1",
+    imagem: "https://archive.org/download/FlashComics001/FlashComics001.jpg",
+    pdf: "https://archive.org/download/FlashComics001/FlashComics001.pdf"
+  },
+  {
+    nome: "Green Lantern #1",
+    imagem: "https://archive.org/download/GreenLantern001/GreenLantern001.jpg",
+    pdf: "https://archive.org/download/GreenLantern001/GreenLantern001.pdf"
+  },
+  {
+    nome: "Marvel Comics #1",
+    imagem: "https://archive.org/download/MarvelComics001/MarvelComics001.jpg",
+    pdf: "https://archive.org/download/MarvelComics001/MarvelComics001.pdf"
+  },
+  {
+    nome: "Detective Comics #38 (Robin estreia)",
+    imagem: "https://archive.org/download/DetectiveComics038/DetectiveComics038.jpg",
+    pdf: "https://archive.org/download/DetectiveComics038/DetectiveComics038.pdf"
   }
 ];
 
@@ -42,6 +62,7 @@ hqs.forEach(hq => {
   `;
   hqList.appendChild(card);
 });
+
 
 
 
